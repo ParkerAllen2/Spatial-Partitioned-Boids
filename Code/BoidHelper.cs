4a0de4291ce7508cdb89a4525f22b5e93a1f99f4
ParkerAllen2 <parker.allen702+1@gmail.com>
2021-01-11 17:09:23 -0500

    main code

---
+++ Code/BoidHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidHelper
+{
+    const int numViewDirections = 300 / 5;
+    public static readonly Vector3[] directions;
+    public static readonly Vector3[] offsets;
+
+    static BoidHelper()
+    {
+        directions = new Vector3[BoidHelper.numViewDirections];
+
+        float angleIncrement = 5;
+
+        for (int i = 0; i < numViewDirections / angleIncrement; i++)
+        {
+            int n = ((i % 2 == 1) ? -1 : 1) * ((i + 1) / 2);
+            directions[i] = Quaternion.Euler(0, 0, n * angleIncrement) * Vector2.up;
+        }
+
+        offsets = new Vector3[2000];
+        for(int i = 0; i < offsets.Length; i++)
+        {
+            offsets[i] = (Vector3)Random.insideUnitCircle;
+        }
+    }
+
+    public static Vector3 GetOffset()
+    {
+        return offsets[Random.Range(0, 2000)];
+    }
+}
---
+++ Code/Cell2.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell2
+{
+    public Boid2 head;
+    public Cell2 nextCell, previousCell;
+    public Grid2 grid;
+
+    public int numOfBoids = 0;
+    Vector3 flockHeading = Vector3.zero;
+    Vector3 flockCentre = Vector3.zero;
+
+    public Vector3 flockHeadingTotal = Vector3.zero;
+    public Vector3 flockCentreTotal = Vector3.zero;
+    public int totalNumOfBoids = 0;
+
+    public List<Cell2> perceptionCells;
+    public List<Cell2> avoidCells;
+
+    public Vector3 cellPosition;
+
+    public void UpdateValues(Vector3 pos, Vector3 up)
+    {
+        flockCentre = new Vector3(pos.x + flockCentre.x, pos.y + flockCentre.y, 0);
+        flockHeading = new Vector3 (up.x + flockHeading.x, up.y + flockHeading.y, 0);
+    }
+
+    public void ResetTotals()
+    {
+        flockCentreTotal = flockCentre;
+        flockHeadingTotal = flockHeading;
+        totalNumOfBoids = numOfBoids;
+    }
+
+    public void CalcValues()
+    {
+        flockCentre = Vector3.zero;
+        flockHeading = Vector3.zero;
+        numOfBoids = 0;
+        Boid2 b = head;
+        while(b != null)
+        {
+            flockCentre += b.position;
+            flockHeading += b.up;
+            numOfBoids += 1;
+            b = b.nextBoid;
+        }
+    }
+
+    public void AddToCells()
+    {
+        foreach (Cell2 c in perceptionCells)
+        {
+            if (c.numOfBoids != 0)
+            {
+                c.Add(numOfBoids, flockHeading, flockCentre);
+            }
+        }
+    }
+
+    public void Add(int f, Vector3 h, Vector3 c)
+    {
+        totalNumOfBoids += f;
+        flockHeadingTotal = new Vector3(h.x + flockHeadingTotal.x, h.y + flockHeadingTotal.y, 0);
+        flockCentreTotal = new Vector3(c.x + flockCentreTotal.x, c.y + flockCentreTotal.y, 0);
+    }
+
+    public void AddBoid(Boid2 b)
+    {
+        b.previousBoid = null;
+        b.nextBoid = head;
+        head = b;
+        if (b.nextBoid != null)
+        {
+            b.nextBoid.previousBoid = b;
+        }
+        b.cell = this;
+        numOfBoids++;
+    }
+
+    public void RemoveBoid(Boid2 b)
+    {
+        if (b.previousBoid != null)
+        {
+            b.previousBoid.nextBoid = b.nextBoid;
+        }
+
+        if (b.nextBoid != null)
+        {
+            b.nextBoid.previousBoid = b.previousBoid;
+        }
+
+        if (head == b)
+        {
+            head = b.nextBoid;
+        }
+        numOfBoids--;
+        if(numOfBoids == 0)
+        {
+            grid.RemoveCell(this);
+        }
+    }
+
+    public void DebugBoidsInCell()
+    {
+        int cc = 0;
+        Boid2 b = head;
+        while (b != null)
+        {
+            if (b == head)
+                Debug.DrawLine(b.position, cellPosition, Color.yellow);
+            else
+                Debug.DrawLine(b.position, cellPosition);
+            b = b.nextBoid;
+            cc++;
+        }
+        Debug.Log("cell = " + cc);
+    }
+
+    public void DebugOtherCells()
+    {
+        foreach (Cell2 ac in perceptionCells)
+        {
+            Debug.DrawLine(cellPosition, ac.cellPosition, Color.blue);
+        }
+        foreach (Cell2 ac in avoidCells)
+        {
+            Debug.DrawLine(cellPosition, ac.cellPosition, Color.red);
+        }
+    }
+}
